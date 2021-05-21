92b69d8895e91edc7f92de23290176b5be6a60fb
No Code, Low Code <dunkey2020@gmail.com>
2021-05-22 02:20:52 +0900

    추가 : Remote

--- app/src/main/java/com/junction/savebears/ChallengeListActivity.kt
+++ app/src/main/java/com/junction/savebears/ChallengeListActivity.kt
@@ -1,17 +1,21 @@
 package com.junction.savebears
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.junction.savebears.base.BaseActivity
 import com.junction.savebears.databinding.ActivityChallengeListBinding
+import com.junction.savebears.remote.retrofit.ApiModule
 
 class ChallengeListActivity : BaseActivity() {
 
     private lateinit var binding: ActivityChallengeListBinding
+    private val saveBearsApi
+        get() = ApiModule.saveBearsApi()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChallengeListBinding.inflate(layoutInflater)
 
+        // TODO with api
+        saveBearsApi
     }
 }
