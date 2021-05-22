ff0eb9fba2fce9248fce846bab6f391bbf54572e
zoown13 <41746120+zoown13@users.noreply.github.com>
2021-05-22 17:35:48 +0900

    amplify 설정 json 활성화

--- app/src/main/java/com/junction/savebears/app/App.kt
+++ app/src/main/java/com/junction/savebears/app/App.kt
@@ -1,6 +1,7 @@
 package com.junction.savebears.app
 
 import android.app.Application
+import android.util.Log
 import androidx.room.Room
 import androidx.viewbinding.BuildConfig
 import com.amplifyframework.AmplifyException
@@ -31,10 +32,11 @@ class App : Application() {
             Amplify.addPlugin(AWSS3StoragePlugin())
             Amplify.configure(applicationContext)
 
-            Timber.i("Initialized Amplify")
+            Log.i("App","Initialized Amplify")
         } catch (error: AmplifyException) {
-            Timber.i("Could not initialize Amplify: $error")
+            Log.e("App","Could not initialize Amplify: error")
         }
+        //uploadFile()
 
     }
 
